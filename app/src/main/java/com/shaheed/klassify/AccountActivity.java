0e41229dd13a7980933413ff065f5147c5d3b3fd
sdewan64 <sdewan64@gmail.com>
2015-01-30 16:43:34 +0600

    Tabs view implemented.

--- app/src/main/java/com/shaheed/klassify/AccountActivity.java
+++ app/src/main/java/com/shaheed/klassify/AccountActivity.java
@@ -1,17 +1,60 @@
 package com.shaheed.klassify;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.shaheed.klassify.adapters.TabsPagerAdapter;
 
-public class AccountActivity extends ActionBarActivity {
+
+public class AccountActivity extends ActionBarActivity implements android.support.v7.app.ActionBar.TabListener{
+
+    private ViewPager viewPager;
+    private TabsPagerAdapter mAdapter;
+    private String[] tabs = { "My Ads", "Wish List", "Edit Information" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        initiateTabsView();
+
+    }
+
+    private void initiateTabsView() {
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(mAdapter);
+        actionBar.setHomeButtonEnabled(false);
+
+        for (String tab_name : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name)
+                    .setTabListener(this));
+        }
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
     }
 
 
@@ -35,5 +78,20 @@ public class AccountActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
     }
 }
